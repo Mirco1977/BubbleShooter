a6e95c1d8c6ca2b08cb51157af8ce9a3dd16ccb4
Mirco Djerdak <djerdakmirco@web.de>
2026-08-24 16:53:01 +0200

    Album fix

--- js/config/albumConfig.js
+++ js/config/albumConfig.js
@@ -7,12 +7,12 @@ export const ALBUM_CONFIG = [
     cover: "assets/albums/swords/album-swords.png",
     completionImage: "assets/albums/swords/album-complete.png",
     cards: [
-      { id: "longsword", number: 78,  name: "Longsword",    unlockLevel: 70,  image: "assets/albums/swords/longsword.png" },
-      { id: "kilij",     number: 167, name: "Kilij",        unlockLevel: 80,  image: "assets/albums/swords/kilij.png" },
-      { id: "gladius",   number: 203, name: "Gladius",      unlockLevel: 90,  image: "assets/albums/swords/gladius.png" },
-      { id: "viking",    number: 314, name: "Viking Sword", unlockLevel: 100, image: "assets/albums/swords/viking-sword.png" },
-      { id: "arming",    number: 319, name: "Arming Sword", unlockLevel: 110, image: "assets/albums/swords/arming-sword.png" },
-      { id: "flamberge", number: 418, name: "Flamberge",    unlockLevel: 120, image: "assets/albums/swords/flamberge.png" }
+      { id: "longsword", number: 78,  name: "Longsword",    unlockLevel: 30,  image: "assets/albums/swords/longsword.png" },
+      { id: "kilij",     number: 167, name: "Kilij",        unlockLevel: 40,  image: "assets/albums/swords/kilij.png" },
+      { id: "gladius",   number: 203, name: "Gladius",      unlockLevel: 50,  image: "assets/albums/swords/gladius.png" },
+      { id: "viking",    number: 314, name: "Viking Sword", unlockLevel: 60, image: "assets/albums/swords/viking-sword.png" },
+      { id: "arming",    number: 319, name: "Arming Sword", unlockLevel: 70, image: "assets/albums/swords/arming-sword.png" },
+      { id: "flamberge", number: 418, name: "Flamberge",    unlockLevel: 80, image: "assets/albums/swords/flamberge.png" }
     ],
     reward: {
       title: "Album-Belohnung",
@@ -33,12 +33,12 @@ export const ALBUM_CONFIG = [
     cover: "assets/albums/crowns/album-complete.png",
     completionImage: "assets/albums/crowns/album-complete.png",
     cards: [
-      { id: "pharao-tutanchamun", number: 1, name: "Krone des Pharao Tutanchamun", unlockLevel: 71, image: "assets/albums/crowns/pharao-tutanchamun.png" },
-      { id: "st-edwards-crown", number: 2, name: "St. Edward’s Crown", unlockLevel: 81, image: "assets/albums/crowns/st-edwards-crown.png" },
-      { id: "moghul-kaiser", number: 3, name: "Krone der Moghul-Kaiser", unlockLevel: 91, image: "assets/albums/crowns/moghul-kaiser.png" },
-      { id: "azteken-kopfschmuck", number: 4, name: "Azteken-Kopfschmuck", unlockLevel: 101, image: "assets/albums/crowns/azteken-kopfschmuck.png" },
-      { id: "krone-von-tonga", number: 5, name: "Krone von Tonga", unlockLevel: 111, image: "assets/albums/crowns/krone-von-tonga.png" },
-      { id: "zulu-koenig", number: 6, name: "Krone des Zulu-Königs", unlockLevel: 121, image: "assets/albums/crowns/zulu-koenig.png" }
+      { id: "pharao-tutanchamun", number: 1, name: "Krone des Pharao Tutanchamun", unlockLevel: 90, image: "assets/albums/crowns/pharao-tutanchamun.png" },
+      { id: "st-edwards-crown", number: 2, name: "St. Edward’s Crown", unlockLevel: 100, image: "assets/albums/crowns/st-edwards-crown.png" },
+      { id: "moghul-kaiser", number: 3, name: "Krone der Moghul-Kaiser", unlockLevel: 110, image: "assets/albums/crowns/moghul-kaiser.png" },
+      { id: "azteken-kopfschmuck", number: 4, name: "Azteken-Kopfschmuck", unlockLevel: 120, image: "assets/albums/crowns/azteken-kopfschmuck.png" },
+      { id: "krone-von-tonga", number: 5, name: "Krone von Tonga", unlockLevel: 130, image: "assets/albums/crowns/krone-von-tonga.png" },
+      { id: "zulu-koenig", number: 6, name: "Krone des Zulu-Königs", unlockLevel: 140, image: "assets/albums/crowns/zulu-koenig.png" }
     ],
     reward: {
       title: "Album-Belohnung",
